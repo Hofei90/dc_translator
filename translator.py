63317bba82baada0a42ed003441ea4ffede0fe34
Hofei90 <29521028+hofei90@users.noreply.github.com>
2022-03-16 10:06:57 +0100

    sprachen ergänzt

--- translator.py
+++ translator.py
@@ -15,43 +15,58 @@ client = discord.Client()
 
 def set_target_language(emoji):
     """
+    Liste verfügbarer Sprachen
     https://www.deepl.com/de/docs-api/translating-text/request/
-    "BG" - Bulgarian - 🇧🇬
-    "CS" - Czech - 🇨🇿
-    "DA" - Danish - 🇩🇰
-    "DE" - German - 🇩🇪
-    "EL" - Greek - 🇬🇷
-    "EN" - English - 🇬🇧 - 🇺🇲
-    "ES" - Spanish - 🇪🇸
-    "ET" - Estonian - 🇪🇪
-    "FI" - Finnish - 🇫🇮
-    "FR" - French - 🇫🇷
-    "HU" - Hungarian - 🇭🇺
-    "IT" - Italian - 🇮🇹
-    "JA" - Japanese - 🇯🇵
-    "LT" - Lithuanian - 🇱🇹
-    "LV" - Latvian - 🇱🇻
-    "NL" - Dutch - 🇳🇱
-    "PL" - Polish - 🇵🇱
-    "PT" - Portuguese - 🇵🇹
-    "RO" - Romanian - 🇷🇴
-    "RU" - Russian - 🇷🇺
-    "SK" - Slovak - 🇸🇰
-    "SL" - Slovenian - 🇸🇮
-    "SV" - Swedish - 🇸🇪
-    "ZH" - Chinese - 🇨🇳
     """
 
-    if emoji == "🇬🇧":  # UK-Englisch
-        target_language = "en-GB"
-    elif emoji == "🇺🇲":  # US-Englisch
-        target_language = "en-US"
-    elif emoji == "🇩🇪":  # Deutsch
-        target_language = "de"
-    elif emoji == "🇷🇺":  # Russisch
-        target_language = "ru"
-    elif emoji == "🇵🇱":  # Polnisch
-        target_language = "pl"
+    if emoji == "🇧🇬":
+        target_language = "bg"  # bulgarisch
+    elif emoji == "🇨🇿":
+        target_language = "cs"  # tschechisch
+    elif emoji == "🇩🇰":
+        target_language = "da"  # dänisch
+    elif emoji == "🇩🇪":
+        target_language = "de"  # deutsch
+    elif emoji == "🇬🇧":
+        target_language = "en-GB"  # uk-englisch
+    elif emoji == "🇺🇲":
+        target_language = "en-US"  # us-englisch
+    elif emoji == "🇪🇸":
+        target_language = "es"  # spanisch
+    elif emoji == "🇪🇪":
+        target_language = "et"  # estnisch
+    elif emoji == "🇫🇮":
+        target_language = "fi"  # finnisch
+    elif emoji == "🇫🇷":
+        target_language = "fr"  # französisch
+    elif emoji == "🇭🇺":
+        target_language = "hu"  # ungarisch
+    elif emoji == "🇮🇹":
+        target_language = "it"  # italienisch
+    elif emoji == "🇯🇵":
+        target_language = "ja"  # japanisch
+    elif emoji == "🇱🇹":
+        target_language = "lt"  # litaunisch
+    elif emoji == "🇱🇻":
+        target_language = "lv"  # lettisch
+    elif emoji == "🇳🇱":
+        target_language = "nl"  # niederländisch
+    elif emoji == "🇵🇱":
+        target_language = "pl"  # polnisch
+    elif emoji == "🇵🇹":
+        target_language = "pt"  # portogisisch
+    elif emoji == "🇷🇴":
+        target_language = "ro"  # rumänisch
+    elif emoji == "🇷🇺":
+        target_language = "ru"  # russisch
+    elif emoji == "🇸🇰":
+        target_language = "sk"  # slovakisch
+    elif emoji == "🇸🇮":
+        target_language = "sl"  # slovenisch
+    elif emoji == "🇸🇪":
+        target_language = "sv"  # schwedisch
+    elif emoji == "🇨🇳":
+        target_language = "zh"  # chinesisch
     else:
         return
     return target_language
@@ -64,7 +79,7 @@ async def on_reaction_add(reaction, _):  # (reation, user)
 
     target_language = set_target_language(str(reaction.emoji))
     if target_language is None:
-        # Kein Emoji zur Übersetzung gewählt
+        # Kein Emoji welches eine übersetzbare Länderflagge ist gewählt
         return
 
     data = {
